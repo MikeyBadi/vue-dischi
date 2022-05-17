cf2cc3de593aa87eb771ab460419bb6d50cfd36c
MikeyBadi <mihaibadia.1999@gmail.com>
2022-05-17 16:21:57 +0200

    Made header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <mbHeader/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import mbHeader from './components/mbHeader.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    mbHeader
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import '../src/assets/style/general'
 </style>
---
+++ src/components/mbHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <header class="d-flex justify-content-between">
+    <img src="../assets/img/logo-small.svg" alt="">
+    <select name="genere" id="" placeholder="Seleziona un genere">
+      <option value="all">Seleziona un genere</option>
+      <option value="rock">Rock</option>
+      <option value="metal">Metal</option>
+    </select>
+  </header>
+</template>
+
+<script>
+export default {
+name: 'mbHeader'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/style/vars';
+  header{
+    padding: 10px 20px;
+    background-color: $contouring;
+
+    img{
+      height: 40px;
+      color:$spoty;
+    }
+  }
+</style>
